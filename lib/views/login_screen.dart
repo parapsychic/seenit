b99a74d0f834d3f8e240898a549d2b4e5d50db08
parapsychic <febinkdominic@outlook.com>
2023-03-08 17:53:21 +0530

    🤩 Started with LoginScreen SafeArea Column TextFormField Text ElevatedButton

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("Login Screen"),
+            TextFormField(),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
